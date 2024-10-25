ce54502fd2a6caa7f98a904104185d11eaca461c
Abdullahs1214 <abdullah.khan.sherwani@gmail.com>
2024-10-25 13:17:11 -0600

    countcities update

--- app/src/main/java/com/example/lab8/City.java
+++ app/src/main/java/com/example/lab8/City.java
@@ -1,5 +1,9 @@
 package com.example.lab8;
 
+import androidx.annotation.Nullable;
+
+import java.util.Objects;
+
 public class City {
 
     private String city;
@@ -16,5 +20,21 @@ public class City {
 
     String getProvinceName(){
         return this.province;
+    }
+    public int compareTo(Object o){
+        City city = (City) o;
+        return this.city.compareTo(city.getCityName());
+    }
+
+    @Override
+    public boolean equals(@Nullable Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        City city1 = (City) o;
+        return Objects.equals(city1.getCityName(),city) && Objects.equals(city1.getProvinceName(), province);
+    }
+    @Override
+    public int hashCode(){
+        return Objects.hash(city,province);
     }
 }
